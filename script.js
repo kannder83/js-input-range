376779e63380135b8618b6093cc692d02cc7ee10
Alejandro Ruiz <kannder.ziur@gmail.com>
2021-03-04 12:44:12 -0500

    Se agrega la funcionalidad del js. Se corrigen los colores.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const weight = document.getElementById("weight");
 const height = document.getElementById("height");
+let heighMeters = 1;
 
 const valueRange = document.querySelector(".container_input");
 
@@ -10,8 +11,10 @@ valueRange.addEventListener("input", (select_value) => {
   }
 
   if (selectRange.target.id == "height") {
+    heighMeters = height.value / 100;
     writeHtml("height", height.value);
   }
+  writeHtml("display_imc", calcImc(weight.value, heighMeters));
 });
 
 function writeHtml(place, value) {
@@ -22,5 +25,26 @@ function writeHtml(place, value) {
     case "height":
       document.getElementById("value_height").innerHTML = `${value} cm`;
       break;
+    case "display_imc":
+      document.getElementById("value_imc").innerHTML = `${value}`;
+      break;
+    case "health":
+      document.getElementById("value_health").innerHTML = `${value}`;
+      break;
   }
+}
+
+function calcImc(weight, heighMeters) {
+  let imc;
+  imc = weight / (heighMeters * heighMeters);
+  if (imc < 18.5) {
+    writeHtml("health", "Peso inferior al normal");
+  } else if (imc > 18.5 && imc < 24.9) {
+    writeHtml("health", "Normal");
+  } else if (imc > 25.0 && imc < 29.9) {
+    writeHtml("health", "Peso superior al normal");
+  } else {
+    writeHtml("health", "Obesidad");
+  }
+  return imc.toFixed(2);
 }
